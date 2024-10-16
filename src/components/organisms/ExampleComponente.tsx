fe1debae513d857cbb3afc0a3346660f652b711c
George Gil Garib <georgegilgarib@MacBook-Pro-de-George.local>
2024-10-16 19:03:19 -0400

    adding test configuration

--- src/components/organisms/ExampleComponente.tsx
+++ src/components/organisms/ExampleComponente.tsx
@@ -3,7 +3,7 @@ import { useTranslation } from "react-i18next";
 const ExampleComponent = () => {
     const { t } = useTranslation()
     return (
-      <div className="p-4  text-blue-900 rounded">
+      <div className="p-4 text-blue-900 rounded">
         <h4>{t('yes')}</h4>
       </div>
     );
---
+++ vitest.config.ts
@@ -0,0 +1,32 @@
+import react from '@vitejs/plugin-react';
+import { mergeConfig, defineConfig, configDefaults } from 'vitest/config';
+import viteConfig from './vite.config';
+
+export default mergeConfig(
+  viteConfig,
+  defineConfig({
+    plugins: [react()],
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      exclude: [...configDefaults.exclude, 'e2e/**'],
+      update: true,
+      outputFile: {
+        markdown: './test-report.md',
+        json: './test-report/test-report.json',
+      },
+      coverage: {
+        all: true,
+        reporter: ['text', 'text-summary', 'json-summary', 'json'],
+        reportOnFailure: true,
+        reportsDirectory: './coverage',
+        thresholds: {
+          lines: 25,
+          statements: 25,
+          functions: 10,
+          branches: 10,
+        },
+      },
+    },
+  }),
+);
